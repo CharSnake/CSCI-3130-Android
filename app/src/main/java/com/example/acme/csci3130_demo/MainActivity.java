76310df75755ec855c815419413a2827f44663d8
CharSnake <32778629+CharSnake@users.noreply.github.com>
2018-01-24 15:54:08 -0400

    Add files via upload

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void changeText(View view) {
+        TextView text = (TextView)findViewById(R.id.textView);
+        EditText edit = (EditText)findViewById(R.id.editText);
+        text.setText(edit.getText());
     }
 }
